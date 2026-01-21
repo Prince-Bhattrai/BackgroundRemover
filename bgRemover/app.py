f2a339eee0e430284d7f93bc5acb51fae3f70bba
Prince Bhattrai <bhattraiprince39@gmail.com>
2026-01-22 03:50:13 +0545

    Update app.py

--- bgRemover/app.py
+++ bgRemover/app.py
@@ -35,10 +35,5 @@ def test():
     return {"Response": "App is running..."}
 
 
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 8000))  
-#     uvicorn.run("app:app", host="0.0.0.0", port=port, workers=1)
-
-
 
 
